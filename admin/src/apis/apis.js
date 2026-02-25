94d3034154488ad75e11992e9c9de17b3dad21c8
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-02-25 22:57:52 +0530

    feat: create api file

---
+++ admin/src/apis/apis.js
@@ -0,0 +1,24 @@
+const commonUrl = 'http://localhost:8080/api/'
+
+const API = {
+    common: {
+        login: `${commonUrl}common/login`,
+        forget_password_send_mail: `${commonUrl}common/forget-password-send-mail`,
+        forget_password_reset: `${commonUrl}common/forget-password-reset`,
+        reg: `${commonUrl}common/reg`,
+        verify: `${commonUrl}common/verify`,
+        getBuses: `${commonUrl}common/get-buses`,
+        getSchedule: `${commonUrl}common/get-schedule`
+    },
+    user: {
+        search: `${commonUrl}user/search-schedule`
+    },
+    admin: {
+        addBus: `${commonUrl}admin/add-bus`,
+        deleteBus: `${commonUrl}admin/delete-bus`,
+        addSchedule: `${commonUrl}admin/create-new-schedule`,
+        deleteSchedule: `${commonUrl}admin/delete-schedule`
+    }
+}
+
+export default API
