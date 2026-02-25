2388203c8dddbaf120b191dc03ec85f0ee959251
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-02-26 02:33:17 +0530

    feat: index with protected routes - client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './index.css'
-import App from './App.jsx'
+
+// auth
+import ProtectedRoute from './auth/ProtectedRoute';
+
+// pages
+import LoginPage from './page/LoginPage';
+import DashboardPage from './page/DashboardPage';
+import RegistrationPage from './page/RegistrationPage';
+import ForgetPasswordPage from './page/ForgetPasswordPage';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Router>
+      <Routes>
+        <Route path='/' element={<LoginPage/>}/>
+        <Route path='/reg' element={<RegistrationPage/>}/>
+        <Route path='/forget-password' element={<ForgetPasswordPage/>}/>
+          <Route path='/dashboard' element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          }/>
+      </Routes>
+    </Router>
+  </StrictMode>
 )
