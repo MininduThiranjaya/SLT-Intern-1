f80e453eda2e7a3af33e642695c03f61f4209eb7
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-02-25 22:58:21 +0530

    feat: main

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './index.css'
-import App from './App.jsx'
+
+// auth
+import ProtectedRoute from './auth/ProtectedRoute';
+
+// pages
+import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Router>
+      <Routes>
+        <Route path='/' element={<LoginPage/>}/>
+          <Route path='/dashboard' element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          }/>
+      </Routes>
+    </Router>
+  </StrictMode>
 )
