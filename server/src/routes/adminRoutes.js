c1723e4d33eb75030a163e3c355c40842ba48ac5
Thiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2026-02-27 14:07:27 +0530

    feat: create new routes

--- server/src/routes/adminRoutes.js
+++ server/src/routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {createSchedule, addBus, deleteBus, deleteSchedule} = require('../services/adminServices')
+const {createSchedule, addBus, deleteBus, deleteSchedule, getUsers, getAllBookings, updateBookingStatus} = require('../services/adminServices')
 const {verifyToken} = require("../auth/auth")
 
 router.post('/add-bus', verifyToken, async (req, res) => {
@@ -20,6 +20,21 @@ router.post('/create-new-schedule', verifyToken, async (req, res) => {
 
 router.delete('/delete-schedule', verifyToken, async (req, res) => {
         const result = await deleteSchedule(req)
+        return res.status(result.statusCode).json({status: result.status, message: result.message ?? null, result: result.data ?? null})
+})
+
+router.get('/get-users', verifyToken, async (req, res) => {
+        const result = await getUsers()
+        return res.status(result.statusCode).json({status: result.status, message: result.message ?? null, result: result.data ?? null})
+})
+
+router.get('/get-all-bookings', verifyToken, async (req, res) => {
+        const result = await getAllBookings()
+        return res.status(result.statusCode).json({status: result.status, message: result.message ?? null, result: result.data ?? null})
+})
+
+router.put('/update-booking-status', verifyToken, async (req, res) => {
+        const result = await updateBookingStatus(req)
         return res.status(result.statusCode).json({status: result.status, message: result.message ?? null, result: result.data ?? null})
 })
 
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const router = express.Router()
-const {searchSchedule, busSeatBooking} = require('../services/userServices')
+const {searchSchedule, busSeatBooking, deleteMyAccount} = require('../services/userServices')
 const {verifyToken} = require("../auth/auth")
 
-router.post('/search-schedule',verifyToken, async (req, res) => {
+router.post('/search-schedule', verifyToken, async (req, res) => {
         const result = await searchSchedule(req)
         return res.status(result.statusCode).json({status: result.status, message: result.message ?? null, result: result.data ?? null})
 })
 
-router.post('/bus-seat-booking',verifyToken, async (req, res) => {
+router.post('/bus-seat-booking', verifyToken, async (req, res) => {
         const result = await busSeatBooking(req)
+        return res.status(result.statusCode).json({status: result.status, message: result.message ?? null, result: result.data ?? null})
+})
+
+router.delete('/delete-my-account', verifyToken, async (req, res) => {
+        const result = await deleteMyAccount(req)
         return res.status(result.statusCode).json({status: result.status, message: result.message ?? null, result: result.data ?? null})
 })
 
